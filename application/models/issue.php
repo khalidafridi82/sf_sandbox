36a9dd8b283979a8c8343a90d794e40e3c51c42c
Khalid Afridi <khalidAfridi@gmail.com>
2014-05-15 17:08:06 +0500

    testing commit + push

---
+++ application/models/issue.php
@@ -0,0 +1,14 @@
+<?php
+
+class Issue extends CI_Model {
+    
+    public $issue_id;
+    public $publication_id;
+    public $issue_number;
+    public $issue_date_publication;
+    public $issue_cover;
+    
+    
+}
+
+
---
+++ application/models/publication.php
@@ -0,0 +1,12 @@
+<?php
+
+ class Publication extends CI_Model {
+     
+     public $publication_id;
+     
+     public $publication_name;
+     
+ }
+
+
+
