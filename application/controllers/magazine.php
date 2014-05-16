ef74890179e64e8461d20e1a4e0627e4b308bd91
Khalid Afridi <khalidAfridi@gmail.com>
2014-05-16 11:52:08 +0500

    Master branch progress

---
+++ application/controllers/magazine.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Magazine extends CI_Controller {
+	
+	public function index () {
+		
+		$this->load->view("magazines");
+                
+		
+		}
+	 
+// testing
+// testing Master Branch stage + commit + push
+}
+
